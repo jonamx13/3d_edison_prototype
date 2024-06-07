54b1c8e3f6df22b8d13ab7fc7c57a8e37174a434
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2024-06-07 14:32:09 -0600

    Selection of animatable parts

--- src/Car.js
+++ src/Car.js
@@ -9,6 +9,7 @@ export function Car() {
         process.env.PUBLIC_URL + "models/edison_proto/edison_proto.gltf"
     );
 
+    // Position Car in space
     useEffect(() => {
         gltf.scene.scale.set(0.5, 0.5, 0.5);
         gltf.scene.position.set(0, .22, 0);
@@ -22,14 +23,39 @@ export function Car() {
         });
     });
 
+    //Animation Controls
     useFrame((state, delta) => {
         let t = state.clock.getElapsedTime();
 
+        // Rotate wheels
         let wheels = gltf.scene.children[3];
         wheels.children[7].rotation.z = t*2; // Back_Left
         wheels.children[8].rotation.z = t*2; // Back_Right
         wheels.children[9].rotation.z = t*2; // Front_Left
         wheels.children[10].rotation.z = t*2; // Front_Right
+
+        // Animate headlights
+        /* let headlights = gltf.scene.children[3];
+        headlights.children[4].rotation.y = t*2; // Lightsbulbs */
+
+        let turningLights = gltf.scene;
+        turningLights.children[4].rotation.y = t * 2; // Left
+        turningLights.children[5].rotation.y = t * 2; // Right
+
+        // Doors
+        let doors = gltf.scene; // Back_Left
+        doors.children[0].rotation.y = t*2 // Front_Left
+        doors.children[1].rotation.y = t*2; // Back_Left
+        doors.children[6].rotation.y = t*2; // Front_Right
+        doors.children[7].rotation.y = t*2; // Back_Right
+
+        // HeadLights
+        let headlight = gltf.scene;
+        headlight.children[2].rotation.y = t*2
+
+        
+
+        
         
     });
 
